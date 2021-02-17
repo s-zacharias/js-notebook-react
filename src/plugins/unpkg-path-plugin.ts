62be4686e888c323c29a1f0b755948009ef13f84
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2021-02-17 13:06:32 -0800

    implement in browser bundling

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,30 @@
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      // handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return { path: args.path, namespace: 'a' };
+      });
+
+      // handle relative paths
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      // handle main file
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
+};
